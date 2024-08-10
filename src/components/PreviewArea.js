8baaa2e34866ff1536414be677f3225daadb1a68
GYashvant <gyashvantkumar@gmail.com>
2024-08-10 12:58:38 +0530

    fixed the color issue

--- src/components/PreviewArea.js
+++ src/components/PreviewArea.js
@@ -1,50 +1,49 @@
-import React, { useRef, useState } from "react";
+import React, { useState } from "react";
 import CatSprite from "./CatSprite";
 
-export default function PreviewArea({ actions, replayAction }) {
+export default function PreviewArea({ actions }) {
   const [position, setPosition] = useState({ x: 0, y: 0, rotation: 0 });
   const [color, setColor] = useState("#FFAB19");
-  const [storeText, setStoreText] = useState('')
+  const [storeText, setStoreText] = useState("");
 
-  const handleSwitchCase = (action, newPosition, newColor) => {
+  const handleSwitchCase = (action, newPosition) => {
     let updatedAction = action
       .toLowerCase()
       .replace(/[^a-zA-Z0-9]+(.)/g, (match, chr) => chr.toUpperCase());
     switch (updatedAction) {
       case "move10Steps":
         newPosition.x += 10;
-        return
+        return;
       case "turn15DegreesLeft":
         newPosition.rotation -= 15;
-        return
+        return;
       case "turn15DegreesRight":
         newPosition.rotation += 15;
-        return
+        return;
       case "sayHello":
-        setStoreText('Hello')
-        return
+        setStoreText("Hello");
+        return;
       case "changeColor":
-        newColor = newColor === "#FFAB19" ? "#0000FF" : "#FFAB19";
-        return
+        let newColor = color === "#FFAB19" ? "#0000FF" : "#FFAB19";
+        setColor(newColor);
+        return;
       default:
-        return
+        return;
     }
   };
 
   const executeActions = (action, mode) => {
     let newPosition = { ...position };
-    let newColor = color;
     mode === "REPLAY"
-      ? handleSwitchCase(action, newPosition, newColor)
+      ? handleSwitchCase(action, newPosition)
       : actions?.forEach((action) => {
-          handleSwitchCase(action, newPosition, newColor);
+          handleSwitchCase(action, newPosition);
         });
     setPosition(newPosition);
-    setColor(newColor);
   };
 
   const replayNthAction = (n) => {
-    if (n >=0 && n < actions.length) {
+    if (n >= 0 && n < actions.length) {
       const action = actions[n];
       executeActions(action, "REPLAY");
     }
@@ -52,7 +51,7 @@ export default function PreviewArea({ actions, replayAction }) {
 
   return (
     <div className="flex-none h-full overflow-y-auto p-2">
-      <CatSprite position={position} color={color} storeText={storeText}/>
+      <CatSprite position={position} color={color} storeText={storeText} />
       <button
         onClick={executeActions}
         className="mt-4 px-4 py-2 bg-blue-500 text-white rounded"
